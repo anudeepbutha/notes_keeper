af72b05173249220d437ca4ba6181ce4f1d937cf
anudeepbutha <111654636+anudeepbutha@users.noreply.github.com>
2025-10-31 20:44:23 +0530

    Add files via upload

--- backend/src/routes/authRouter.js
+++ backend/src/routes/authRouter.js
@@ -2,10 +2,10 @@ import express from "express";
 import { signUp, signIn, logout, verifyAuth } from "../controller/authController.js";
 import { authenticate } from "../middleware/authMiddleware.js";
 
-const router = express.Router();
+const authRouter = express.Router();
 
-router.post("/signup", signUp);
-router.post("/signin", signIn);
-router.post("/logout", authenticate, logout);
-router.get("/verify", authenticate, verifyAuth);
-export default router;
+authRouter.post("/signup", signUp);
+authRouter.post("/signin", signIn);
+authRouter.post("/logout", authenticate, logout);
+authRouter.get("/verify", authenticate, verifyAuth);
+export default authRouter;
